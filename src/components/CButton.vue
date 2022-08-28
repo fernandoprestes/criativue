1f7d8a8f5ac77258787ab37a1604f9695e004449
fernandoprestes <fernandoprestesc@gmail.com>
2022-08-28 15:00:38 -0300

    add button component

---
+++ src/components/CButton.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+  interface Props {
+    variant: 'primary' | 'secondary' | 'outline';
+    label: string;
+  }
+  defineProps<Props>();
+
+  const context = {
+    primary: '--primary',
+    secondary: '--secondary',
+    outline: '--outline',
+  };
+</script>
+<template>
+  <button
+    :class="['base-button ', context[variant]]"
+    v-bind="$attrs"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+  .base-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 12px 16px;
+    color: $color-accent;
+    text-align: center;
+    cursor: pointer;
+    background: transparent;
+    border: 1px solid transparent;
+    border-radius: $radius-medium;
+    transition: all $transition-duration $easing;
+
+    &:hover {
+      filter: brightness(1.1);
+    }
+
+    &.--primary {
+      background: $color-primary;
+    }
+
+    &.--secondary {
+      background: $color-secondary;
+    }
+
+    &.--outline {
+      color: $color-primary;
+      border: 1px solid $color-primary;
+    }
+
+    &.--outline:hover {
+      color: $color-accent;
+      background: $color-primary;
+    }
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+  import CButton from '~/components/CButton.vue';
+</script>
+
+<template>
+  <div class="wrapper-div">
+    <h2>Buttons</h2>
+    <div class="button-group">
+      <CButton
+        label="Primary"
+        variant="primary"
+      />
+      <CButton
+        label="Secondary"
+        variant="secondary"
+      />
+      <CButton
+        label="Outline"
+        variant="outline"
+      />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .wrapper-div {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+  }
+
+  .button-group {
+    display: flex;
+    gap: 16px;
+  }
+</style>
