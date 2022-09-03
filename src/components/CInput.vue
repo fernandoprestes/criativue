acb1358233e2317df717389f17f11d56ff5e4f54
fernandoprestes <fernandoprestesc@gmail.com>
2022-09-03 14:56:42 -0300

    add input component

---
+++ src/components/CInput.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+  interface Props {
+    label: string;
+    modelValue?: string | number | null;
+  }
+  defineProps<Props>();
+
+  const emit = defineEmits(['update:modelValue']);
+  const onChangeInput = (event: Event) => {
+    emit('update:modelValue', (event.target as HTMLInputElement).value);
+  };
+</script>
+<template>
+  <div class="input-group-wrapper">
+    <input
+      :id="label"
+      class="input-base"
+      v-bind="$attrs"
+      :value="modelValue"
+      :placeholder="label"
+      onblur="this.setAttribute('data-content-valid', 'invalid');"
+      @input="onChangeInput"
+    />
+    <label class="input-label">{{ label }}</label>
+    <span class="has-error-icon">E</span>
+    <span class="input-border"></span>
+    <span class="has-error">Este campo é obrigatório</span>
+  </div>
+</template>
+<style scoped lang="scss">
+  .input-group-wrapper {
+    position: relative;
+    flex: 1 1 auto;
+    padding: 12px 16px 8px;
+    margin-bottom: 16px;
+    font-size: 16px;
+    background: #fff;
+    border-radius: 8px 8px 0 0;
+  }
+
+  .input-group-wrapper:has(~ .has-error) {
+    margin-bottom: 25px;
+  }
+
+  .input-base {
+    display: block;
+    width: 100%;
+    padding: 12px 0 0;
+    background: transparent;
+    border: 0;
+
+    &:focus {
+      outline: 0;
+    }
+
+    &::placeholder {
+      color: transparent;
+    }
+  }
+
+  .input-group-wrapper :where([disabled='true']),
+  .input-group-wrapper :where([disabled='']) {
+    background: #edfff6;
+  }
+
+  .input-label {
+    position: absolute;
+    top: 18px;
+    left: 16px;
+    width: 80%;
+    overflow: hidden;
+    color: #8f9994;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    pointer-events: none;
+    -moz-transition: 0.2s ease all;
+    -webkit-transition: 0.2s ease all;
+    transition: 0.2s ease all;
+  }
+
+  .input-base:focus ~ .input-label,
+  .input-base:not(:placeholder-shown) ~ .input-label {
+    top: 6px;
+    left: 15px;
+    font-size: 12px;
+    color: $color-primary;
+  }
+
+  .input-border {
+    position: absolute;
+    bottom: 4px;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    pointer-events: none;
+    background: $color-border;
+  }
+
+  .has-error-icon,
+  .has-error {
+    display: none;
+  }
+
+  .input-base:where([required]):where([data-content-valid='invalid']) ~ .input-border {
+    background: #e6522d;
+  }
+
+  .input-base:focus ~ .input-border,
+  .input-base:not(:placeholder-shown) ~ .input-border {
+    background: $color-primary;
+  }
+
+  .input-group-wrapper .input-base:where([required]):where([data-content-valid='invalid']) ~ .has-error {
+    position: absolute;
+    top: 48px;
+    left: 0;
+    display: block;
+    font-size: 14px;
+    color: #e6522d;
+  }
+
+  .input-group-wrapper .input-base:where([required]):where([data-content-valid='invalid']) ~ .has-error-icon {
+    position: absolute;
+    top: 20px;
+    right: 0;
+    display: block;
+    font-size: 14px;
+    color: #e6522d;
+  }
+
+  .input-base:focus ~ .has-error,
+  .input-base:not(:placeholder-shown) ~ .has-error {
+    display: none;
+  }
+
+  .input-base:focus ~ .has-error-icon,
+  .input-base:not(:placeholder-shown) ~ .has-error-icon {
+    display: none;
+  }
+
+  .input-group-wrapper .input-base:not(:placeholder-shown) ~ .has-error,
+  .input-group-wrapper .input-base:not(:placeholder-shown) ~ .has-error-icon {
+    display: none;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <script lang="ts" setup>
+  import { ref } from 'vue';
   import CButton from '~/components/CButton.vue';
+  import CInput from '../components/CInput.vue';
+
+  const name = ref('');
+  const age = ref();
 </script>
 
 <template>
@@ -20,6 +25,21 @@
       />
     </div>
   </div>
+  <div class="wrapper-div">
+    <h2>Inputs</h2>
+    <div class="button-group">
+      <CInput
+        v-model="name"
+        label="Nome"
+      />
+      <CInput
+        v-model="age"
+        required
+        type="number"
+        label="Idade obrigatório"
+      />
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
   .wrapper-div {
@@ -30,6 +50,7 @@
 
   .button-group {
     display: flex;
-    gap: 16px;
+    flex-wrap: wrap;
+    gap: 8px 16px;
   }
 </style>
